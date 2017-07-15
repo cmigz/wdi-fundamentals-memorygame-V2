236bab0dcbaf09a0d294b93bf776b9b32a6f48a7
cmigz <cmignacca@gmail.com>
2017-07-15 16:47:06 -0400

    Unit 8 Conclusion - Created Game Logic

--- memory-game-V2/js/main.js
+++ memory-game-V2/js/main.js
@@ -1,9 +1,22 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You Found a Match!");
+  } else {
+    alert("Sorry try again");
+  }
+  
